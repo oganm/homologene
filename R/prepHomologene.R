3a5455ed739d37f887f7454a353cf94f7e384ee1
Ogan Mancarci <ogan.mancarcii@gmail.com>
2015-12-17 19:28:29 -0800

    now returns strings instead of factors

--- R/prepHomologene.R
+++ R/prepHomologene.R
@@ -8,7 +8,7 @@ prepHomologene = function(){
     directory = 'data/homologene.tsv'
     homoloGeneTarget <<- directory
     download.file(url = "ftp://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data", destfile = 'homologene.data')
-    homologene = read.table('homologene.data',sep ='\t',quote='')
+    homologene = read.table('homologene.data',sep ='\t',quote='',stringsAsFactors = FALSE)
     names(homologene) = c('HID','Taxonomy','Gene.ID','Gene.Symbol','Protein.GI','Protein.Accession')
     
     homologeneData = homologene %>% filter(Taxonomy %in% wantedTax$taxid) %>% select(HID,Gene.Symbol,Taxonomy) %>% unique %>% arrange(Taxonomy)
