5fc2574a6c18465886eda41dbb021c2c660e93dc
OganM <ogan.mancarcii@gmail.com>
2017-03-08 13:13:51 -0800

    almost complete automatic updater

--- process/prepHomologene.R
+++ process/prepHomologene.R
@@ -1,15 +1,51 @@
-
+library(magrittr)
 library(dplyr)
+library(data.table)
+library(git2r)
+devtools::use_data_raw()
+# taxes = read.table('ftp://ftp.ncbi.nlm.nih.gov/mmdb/pdbeast/tax.table',sep = '')
 
-wantedTax = read.table('data-raw/tax_report.txt',header=T, sep='|')
+# download taxonomy data if it's not already there
+if(!file.exists('data-raw/taxdump/names.dmp')){
+    download.file(url ='ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz', destfile = "data-raw/taxtump.tar.gz")
+    dir.create('data-raw/taxdump', showWarnings = FALSE)
+    untar('data-raw/taxtump.tar.gz',exdir = 'data-raw/taxdump/')
+}
+taxData = fread('data-raw/taxdump/names.dmp',data.table=FALSE)
+taxData = taxData[c(1,3,5,7)]
+names(taxData) = c('tax_id','name_txt','unique_name','name_class')
+taxData %<>% filter(name_txt %in% c('Homo sapiens',
+                                   'Mus musculus',
+                                   'Rattus norvegicus',
+                                   'Danio rerio',
+                                   'Caenorhabditis elegans',
+                                   'Drosophila melanogaster',
+                                   'Rhesus macaque'))
 
 
-directory = 'data-raw/homologene.tsv'
-homoloGeneTarget <<- directory
-download.file(url = "ftp://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data", destfile = 'data-raw/homologene.data')
-homologene = read.table('data-raw/homologene.data',sep ='\t',quote='',stringsAsFactors = FALSE)
-names(homologene) = c('HID','Taxonomy','Gene.ID','Gene.Symbol','Protein.GI','Protein.Accession')
 
-homologeneData = homologene %>% filter(Taxonomy %in% wantedTax$taxid) %>% select(HID,Gene.Symbol,Taxonomy) %>% unique %>% arrange(Taxonomy)
+homologeneVersion = readLines('ftp://ftp.ncbi.nih.gov/pub/HomoloGene/current/RELEASE_NUMBER') %>% as.integer
 
-use_data(homologeneData, overwrite= TRUE)
+# if the release is new, update
+if(homologeneVersion!=readLines('data-raw/release')){
+    download.file(url = "ftp://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data", destfile = 'data-raw/homologene.data')
+    homologene = fread('data-raw/homologene.data',sep ='\t',quote='',stringsAsFactors = FALSE,data.table = FALSE)
+    names(homologene) = c('HID','Taxonomy','Gene.ID','Gene.Symbol','Protein.GI','Protein.Accession')
+    homologeneData = homologene %>% filter(Taxonomy %in% taxData$tax_id) %>% select(HID,Gene.Symbol,Taxonomy) %>% unique %>% arrange(Taxonomy)
+    write.table(homologeneData,file = 'data-raw/homologeneData.tsv',sep='\t', row.names=FALSE)
+    devtools::use_data(homologeneData, overwrite= TRUE)
+    devtools::use_data(homologeneVersion, overwrite= TRUE)
+    repo = repository('.')
+    git2r::add(repo,'data/homologeneData.rda')
+    git2r::add(repo,'data/homologeneVersion.rda')
+    git2r::add(repo,'data-raw/homologeneData.tsv')
+    git2r::add(repo,'man/homologeneData.Rd')
+    git2r::add(repo,'data-raw/release')
+    git2r::commit(repo,message = paste('Automatic update to version',homologeneVersion))
+
+    writeLines(releaseNo,con = 'data-raw/release')
+    
+}
+
+
+
