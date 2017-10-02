7ee1683ee4590ce856b60b711765d8463a1e0ead
OganM <ogan.mancarcii@gmail.com>
2017-10-02 11:52:22 -0700

    minor edit to test something

--- tests/testthat/test_homologene.R
+++ tests/testthat/test_homologene.R
@@ -21,6 +21,6 @@ test_that('Other species',{
 test_that('Detached behaviour',{
     detach("package:homologene", unload=TRUE)
     expect_that(homologene::mouse2human(c('Eno2','Mog'))$humanGene,equals(c('ENO2','MOG')))
-    expect_that(dim(homologene::mouse2human(c('lolwut'))), equals(c(0,2)))
+    expect_that(dim(homologene::human2mouse(c('lolwut'))), equals(c(0,2)))
 })
 
