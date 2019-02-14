246cd18816df3405e8aaa7fb20cdbae9b932268a
OganM <ogan.mancarcii@gmail.com>
2019-02-13 16:30:39 -0800

    homologeneData2 automatic update

--- process/prepHomologene2.R
+++ process/prepHomologene2.R
@@ -134,7 +134,8 @@ devtools::use_data(homologeneData2,overwrite = TRUE)
 glue('
 #\' homologeneData2
 #\'
-#\' A modified copy of the homologene database where the gene IDs and symbols are updated when necesary. 
+#\' A modified copy of the homologene database. Homologene was updated at 2014 and many of its gene IDs and
+#\' symbols are out of date. Here the IDs and symbols are replaced with their most current version
 #\' Last update: {date()}
 "homologeneData2"
 ') %>% 
