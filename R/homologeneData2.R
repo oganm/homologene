454303d24ed3fd95c91d7ed62358c054312ecead
OganM <ogan.mancarcii@gmail.com>
2019-03-11 15:15:07 -0700

    homologeneData2 automatic update

--- R/homologeneData2.R
+++ R/homologeneData2.R
@@ -2,5 +2,5 @@
 #'
 #' A modified copy of the homologene database. Homologene was updated at 2014 and many of its gene IDs and
 #' symbols are out of date. Here the IDs and symbols are replaced with their most current version
-#' Last update: Mon Feb 25 16:42:02 2019
+#' Last update: Mon Mar 11 15:14:51 2019
 "homologeneData2"
