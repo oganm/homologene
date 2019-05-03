26ebb9d943003ed0b6df76360b3d15fdd73843d1
OganM <ogan.mancarcii@gmail.com>
2019-05-03 15:29:39 -0700

    configurable delay for future proofing. reading robots at each execution is counter intuitive

--- R/diopt.R
+++ R/diopt.R
@@ -4,19 +4,22 @@
 #' 
 #' Query DIOPT database (\url{https://www.flyrnai.org/cgi-bin/DRSC_orthologs.pl}) for orthologues.
 #' DIOPT database uses multiple tools to find gene orthologues. Sadly they don't have an
-#' API so this function queries by visiting the site and filling up the form while obeying
-#' their robots.txt. All queries will take a minimum of 10 seconds due to crawl delay.
+#' API so this function queries by visiting the site and filling up the form. By default
+#' each query will take a minimum of 10 seconds due to \code{delay} parameter. This
+#' is taken from their robots.txt
 #'
 #' @param genes  A vector of gene identifiers. Anything that DIOPT accepts
 #' @param inTax taxid of the species that the input genes are coming from
 #' @param outTax taxid of the species that you are seeking homology
+#' @param delay How many seconds of delay should be between queries. Default is 10
+#' based on the robots.txt at the time this function is written.
 #'
 #' @return
 #' @export
 #'
-diopt = function(genes, inTax, outTax){
-    rtxt = robotstxt::robotstxt(domain = "flyrnai.org")
-    delay = rtxt$crawl_delay %>% filter(useragent =='*') %$% value %>% as.integer()
+diopt = function(genes, inTax, outTax, delay = 10){
+    # rtxt = robotstxt::robotstxt(domain = "flyrnai.org")
+    # delay = rtxt$crawl_delay %>% filter(useragent =='*') %$% value %>% as.integer()
     session = rvest::html_session('https://www.flyrnai.org/cgi-bin/DRSC_orthologs.pl')
     form = rvest::html_form(session)[[1]]
     
