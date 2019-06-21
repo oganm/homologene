0966adc0ed548a3c2b42d36904b5501167126f84
OganM <ogan.mancarcii@gmail.com>
2019-06-21 15:46:08 -0700

    get all diopt

--- process/dioptMemory.R
+++ process/dioptMemory.R
@@ -4,11 +4,30 @@ session = rvest::html_session('https://www.flyrnai.org/cgi-bin/DRSC_orthologs.pl
 form = rvest::html_form(session)[[1]]
 dioptSpecies = taxData %>% filter(tax_id %in% form$fields$input_species$options)
 
+dioptSpecies$common_name = form$fields$input_species$options %>%
+    {.[match(dioptSpecies$tax_id,.)]} %>%
+    names %>% stringr::str_extract('(?<=\\().*?(?=\\))')
+
+aliquot = function(vector, alisize){
+    vector %>% 
+        split(.,rep_len(1:(floor(length(.)/alisize)),length(.)))
+}
 
 
-homologeneData2 %>% filter(Taxonomy == 10090) -> speciesGenes
-
-
-dioptOut = diopt(speciesGenes$Gene.ID[1:100],inTax = 10090, outTax = '0',delay = 1)
-
-out = diopt(c('GZMH'),inTax = 9606, outTax = 10090)
+dir.create('data-raw/diopt')
+dioptSpecies$tax_id %>% lapply(function(taxID){
+    homologeneData2 %>% 
+        filter(Taxonomy == taxID) -> 
+        speciesGenes
+    
+    speciesGenes$Gene.ID %>% 
+        aliquot(15) %>%
+        {.[]} %>% 
+        lapply(function(x){
+            diopt(x,inTax = 10090,outTax ='0',delay = 10)
+        }) -> aliquotDiopt
+    
+    dioptOut = aliquotDiopt %>% do.call(rbind,.)
+    readr::write_tsv(dioptOut,path = glue::glue('data-raw/diopt/{taxID}'))
+    
+})
