1593c1a734df1d0dffae061a84fffbe3f5665981
OganM <ogan.mancarcii@gmail.com>
2019-06-24 16:53:50 -0700

    data from this weekends auto-update, commit seems to be failing in prepHomologene2.R

--- R/homologeneData2.R
+++ R/homologeneData2.R
@@ -2,5 +2,5 @@
 #'
 #' A modified copy of the homologene database. Homologene was updated at 2014 and many of its gene IDs and
 #' symbols are out of date. Here the IDs and symbols are replaced with their most current version
-#' Last update: Thu Jun 20 14:46:25 2019
+#' Last update: Sun Jun 23 06:43:12 2019
 "homologeneData2"
