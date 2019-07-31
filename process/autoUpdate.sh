d83d8363e927dbb8310dd5f31c66234dd87b58c5
OganM <ogan.mancarcii@gmail.com>
2019-07-31 14:13:57 -0700

    keep update logs to identify why pushes fail

--- process/autoUpdate.sh
+++ process/autoUpdate.sh
@@ -3,5 +3,5 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$parent_path"
 cd ..
-Rscript 'process/prepHomologene.R'
-Rscript 'process/prepHomologene2.R'
+Rscript 'process/prepHomologene.R' > process/prepLog
+Rscript 'process/prepHomologene2.R' > process/prepLog2
