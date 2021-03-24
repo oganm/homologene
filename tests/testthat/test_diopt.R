ef69db612bcbf0955127c9f903dfb0ed1c2a9600
OganM <ogan.mancarcii@gmail.com>
2021-03-23 17:55:23 -0700

    fix for the test just in case

--- tests/testthat/test_diopt.R
+++ tests/testthat/test_diopt.R
@@ -2,7 +2,9 @@ context('diopt')
 
 
 test_that('DIOPT',{
+    httr::set_config(httr::config(ssl_verifypeer = 0L))
+    
     out = diopt(c('GZMH'),inTax = 9606, outTax = 10090)
     
     expect_true(all(c('Gzmd','Gzme','Gzmg','Gzmf') %in% out$`Mouse Symbol`))
-})    
+})
