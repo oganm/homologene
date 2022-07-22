36c6c6d86df81a1d551f1b141931b927aec11ffa
OganM <ogan.mancarcii@gmail.com>
2022-07-21 17:34:47 -0700

    maintenance

--- process/autoUpdate.sh
+++ process/autoUpdate.sh
@@ -3,5 +3,5 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$parent_path"
 cd ..
-Rscript 'process/prepHomologene.R' > process/prepLog
-Rscript 'process/prepHomologene2.R' > process/prepLog2
+Rscript 'process/prepHomologene.R' > process/prepLog 2>process/prepLogErr
+Rscript 'process/prepHomologene2.R' > process/prepLog2 2>process/prepLog2Err
